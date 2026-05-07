eae64063a219b7ebb01cb6298280cc2e22890415
Tran Quang Vinh Dinh Quoc Huy <vinhtransn1998@gmail.com>
2026-05-07 08:56:23 +0700

    push project to Github

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ITSupportForum.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
